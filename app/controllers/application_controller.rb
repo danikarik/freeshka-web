23c4e4599f32f88787dfde49506075bed16b4ed3
danikarik <baitursynov92@gmail.com>
2020-03-23 22:31:35 +0600

    Fix layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
+  # def after_sign_in_path_for(resource)
+  #   stored_location_for(resource) || me_path_url
+  # end
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      'base'
+    else
+      'application'
+    end
+  end
 end

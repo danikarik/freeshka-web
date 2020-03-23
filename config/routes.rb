2fbf61988301d57d3317f466d7f5904d4f37d3f1
danikarik <baitursynov92@gmail.com>
2020-03-23 17:33:52 +0600

    Update devise prefix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "home#index"
+
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login',
+                                                 sign_up: 'register'
+                                                 sign_out: 'logout' }
 
   get 'home/index'
 end

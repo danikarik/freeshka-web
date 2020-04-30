80c7487eb45af5075b032e50d8bc23350f436821
danikarik <baitursynov92@gmail.com>
2020-05-01 02:43:25 +0600

    Add avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    update_keys = [:email, :name, :description, :org_form_id,
+    update_keys = [:email, :name, :description, :org_form_id, :avatar,
                    :password, :password_confirmation, :current_password]
 
     devise_parameter_sanitizer.permit(:account_update, keys: update_keys)
